6778a89bc461319039911deb41c6e8a8dc93fc80
lpalanti <lucaspalanti@hotmail.com>
2025-04-27 08:38:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ CSV_FILE = "prompts_database_complete.csv"
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions"
 
+print("API KEY:", os.getenv("GROQ_API_KEY"))
+
 # Modos de operação
 DEBUG = True  # Altere para False em produção
 
